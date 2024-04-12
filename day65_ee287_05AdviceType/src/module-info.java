e3040366bae91643687c7bb9a6840dde3cd2a3cb
wlf94904 <2993721735@qq.com>
2024-04-12 12:45:19 +0800

    注释

---
+++ day65_ee287_05AdviceType/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author 86131
+ *
+ */
+module day65_ee287_04springAOP {
+	requires spring.context;
+	requires aspectjweaver;
+}
---
+++ day66_ee287_01jdbctemplate/src/com/itheima/jdbctemplate/JdbcTemplateDemo1.java
@@ -0,0 +1,20 @@
+package com.itheima.jdbctemplate;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * JdbcTemplate的最基本用法
+ * @author 86131
+ *
+ */
+public class JdbcTemplateDemo1 {
+
+	public static void main(String[] args) {
+//		1.获取对象
+		JdbcTemplate jt=new JdbcTemplate();
+//		2.执行操作
+		
+
+	}
+
+}
